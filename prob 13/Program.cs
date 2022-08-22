286abb1ddfda21cd0ce44421124e60529efdf355
username <username@cognizant.com>
2022-08-22 21:28:21 +0530

    Add project files.

---
+++ prob 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {1,3,5,200,22,2244,213,111};
+
+            int res = 100;
+            Console.WriteLine("Given number {0}: ", res);
+            Console.WriteLine("Numbers larger than {0} = ", res);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > res)
+                {
+                    Console.WriteLine(arr[i]);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
